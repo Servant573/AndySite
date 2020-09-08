659ede5822790bb2746c012491f5d7188ee09187
Servant573 <vlaad2012@mail.ru>
2020-09-08 21:08:31 +0300

    remove jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-    $('.header_burger').click(function(event) {
-        $('.header_burger').toggleClass('active')
-        $('nav').toggleClass('active')
-        $('body').toggleClass('lock')
-    $('.link-nav').click(function() {
-        $('nav').removeClass('active')
-        $('.header_burger').removeClass('active')
-    })
-    })
-})
+const headerBurger = document.querySelector('.header_burger')
+const navTag = document.querySelector('nav')
+const body = document.querySelector('body')
+const linkNav = navTag.querySelectorAll('.link-nav')
+
+window.onload = function() {
+    headerBurger.onclick = function() {
+        headerBurger.classList.toggle('active')
+        navTag.classList.toggle('active')
+        body.classList.toggle('lock')
+    }
+    for(let i=0; i<linkNav.length; i++) {
+        linkNav[i].onclick = function() {
+            navTag.classList.remove('active')
+            headerBurger.classList.remove('active')  
+        }
+    }
+ }
